960d64673de9a19f750cec30c129c852c7c315db
Ashish Dwivedi <ashishdwivedi7654321@gmail.com>
2026-02-17 10:22:52 +0530

    Working with txt  file - basics

---
+++ WorkingOnFile-1.js
@@ -0,0 +1,20 @@
+//Writing to a file
+const fs = require('fs');
+fs.writeFile('example01.txt', 'This is an example of writing to a file in Node.js', (err) => {
+    if (err) {
+        console.log('Error writing to file:');
+    } else {
+        console.log('File has been written successfully');
+    }
+});
+
+//Reading from a file
+fs.readFile('example01.txt', 'utf8', (err, data) => {
+    if (err) {  
+        console.log('Error reading file:');
+    } else {
+        console.log('File content:',data);
+    }
+}); 
+
+    

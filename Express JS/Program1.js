05ef32b6b0d2e0bd6c728b0c703356e7a28228d8
Ashish Dwivedi <ashishdwivedi7654321@gmail.com>
2026-03-18 19:27:42 +0530

    Working with Express.js

---
+++ Express JS/Program1.js
@@ -0,0 +1,17 @@
+//Creating a simple Express server
+
+//Importing the Express module
+const express = require('express');
+
+//Creating an instance of the Express application
+const app = express();
+
+//Defining a route for the root URL
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+//Starting the server and listening on port 3000
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});

2b8d932ad57ef481642750f0ace4b034952ec94b
Ashish Dwivedi <ashishdwivedi7654321@gmail.com>
2026-03-20 19:23:17 +0530

    Update Program4.js

--- EXPRESS FRAMEWORK/Program4.js
+++ EXPRESS FRAMEWORK/Program4.js
@@ -2,3 +2,16 @@
 
 //Importing the Express module
 const express = require('express');
+
+//Creating an instance of the Express application
+const app = express();
+
+//Defining a route that accepts query parameters
+app.get('/search', (req, res) => {
+    const query = req.query.q; // Accessing the 'q' query parameter
+    if (query) {
+        res.send(`You searched for: ${query}`);
+    } else {
+        res.send('Please provide a search query using the "q" parameter.');
+    }   
+});
